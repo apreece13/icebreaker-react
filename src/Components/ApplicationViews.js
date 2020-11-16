58f3f2a270c1b06e6f24621183fba6be723acbd2
Ashon Woodbury <a.woodbury01@gmail.com>
2020-11-16 16:13:26 -0500

    created ApplicationViews.js, Icebreaker.js, Navbar.js. Next steps: populate Navbar

---
+++ src/Components/ApplicationViews.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+
+class ApplicationViews extends Component {
+    render() {
+        return (
+          <React.Fragment>
+{/*             <Route
+              exact
+              path="/home"
+              render={(props) => {
+                  return <Home {...props} />;
+              }}
+            />
+            <Route
+              exact
+              path="/companies"
+              render={(props) => {
+                  return <Companies {...props} />;
+              }}
+            />
+            <Route
+              exact
+              path="/job-seekers"
+              render={(props) => {
+                  return <JobSeekers {...props} />;
+              }}
+            />
+            <Route
+              exact
+              path="/forums"
+              render={(props) => {
+                  return <Forums {...props} />;
+              }}
+            />
+            <Route
+              exact
+              path="/job-seekers"
+              render={(props) => {
+                  return <JobSeekers {...props} />;
+              }}
+            />
+            <Route
+              exact
+              path="/testimonials"
+              render={(props) => {
+                  return <Testimonials {...props} />;
+              }}
+            />
+            <Route
+              exact
+              path="/bookmarks"
+              render={(props) => {
+                  return <Bookmarks {...props} />;
+              }}
+            />
+            <Route
+              exact
+              path="/profile"
+              render={(props) => {
+                  return <Profile {...props} />;
+              }}
+            />
+ */}            </React.Fragment>
+        );
+      }
+}
+
+export default ApplicationViews;
---
+++ src/Components/Icebreaker.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+/* import Navbar from "./nav/Navbar";
+ */import ApplicationViews from "./ApplicationViews";
+
+
+class Icebreaker extends Component {
+
+      render() {
+        return (
+          <>
+{/*             <Navbar />
+ */}              <ApplicationViews />
+          </>
+        );
+      }
+}
+
+export default Icebreaker;
